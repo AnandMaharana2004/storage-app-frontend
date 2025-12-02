1d6bfb6c4f6b9170c490fd85768463b6a17b92fa
anand maharana <anandmaharana427@gmail.com>
2025-12-02 23:11:42 +0530

    feat: setup vite project with prettier and husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation changes
+        'style',    // Code style changes (formatting)
+        'refactor', // Code refactoring
+        'perf',     // Performance improvements
+        'test',     // Tests
+        'build',    // Build system
+        'ci',       // CI/CD changes
+        'chore',    // Maintenance
+        'revert',   // Revert previous commit
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100],
+  },
+};
