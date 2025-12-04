8974a6309085e5b78a25a7aa74f6c7ae2eede1b2
anand maharana <anandmaharana427@gmail.com>
2025-12-04 14:57:02 +0530

    feat: implement routing with BrowserRouter and define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import LandingPage from "./components/pages/landingPage";
+import LoginPage from "./components/pages/loginPage";
+import RegisterPage from "./components/pages/registerPage";
+import DashboardPage from "./components/pages/dashboardPage";
+import SettingsPage from "./components/pages/settingPage";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <h1 className="text-7xl text-black">Under constraction</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
